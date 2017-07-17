4d86a9a9d4a6ace5eb2dc7c99b03c13cc5d96fcc
KunnyChen <abc582700457@qq.com>
2017-07-17 16:45:10 +0800

    ajax请求

--- Ajax请求.js
+++ Ajax请求.js
@@ -1,8 +1,52 @@
 /*
-* @Author: KunnyChen
-* @Date:   2017-07-15 11:45:20
-* @Last Modified by:   KunnyChen
-* @Last Modified time: 2017-07-15 12:10:35
-*/
+ * @Author: KunnyChen
+ * @Date:   2017-07-15 11:45:20
+ * @Last Modified by:   KunnyChen
+ * @Last Modified time: 2017-07-17 16:42:30
+ */
 
 'use strict';
+export default (type = 'GET', url = '', data = {}, async = true) => {
+    return new Promise((resolve, reject) => { //定义一个promise
+        type = type.toUpperCase();
+
+        let requestObj;
+        if (window.XMLHttpRequest) {
+            requestObj = new XMLHttpRequest();
+        } else {
+            requestObj = new ActiveXObject;
+        }
+
+        if (type == 'GET') {
+            let dataStr = ''; //数据拼接字符串
+            Object.keys(data).forEach(key => {
+                dataStr += key + '=' + data[key] + '&';
+            })
+            dataStr = dataStr.substr(0, dataStr.lastIndexOf('&'));
+            url = url + '?' + dataStr;
+            requestObj.open(type, url, async);
+            requestObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            requestObj.send();
+        } else if (type == 'POST') {
+            requestObj.open(type, url, async);
+            requestObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            requestObj.send(JSON.stringify(data));
+        } else {
+            reject('error type');
+        }
+
+        requestObj.onreadystatechange = () => {
+            if (requestObj.readyState == 4) {
+                if (requestObj.status == 200) {
+                    let obj = requestObj.response
+                    if (typeof obj !== 'object') {
+                        obj = JSON.parse(obj);
+                    }
+                    resolve(obj);
+                } else {
+                    reject(requestObj);
+                }
+            }
+        }
+    })
+}
